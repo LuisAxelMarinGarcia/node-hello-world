91d47bf15ddd5a26f0cc35666b794f7440de9ac4
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2024-06-05 15:15:22 -0600

    aaa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hola mundo');
+    res.send('Hola mundo!');
 });
 
 app.listen(port, '0.0.0.0', () => {
