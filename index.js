72cc0402019c3cd0088fc1c0947adf2c2e70eac5
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2024-06-05 23:56:58 -0600

    Initial commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello, World!');
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
